78e39991b6820277eb42ee6198234c228b56d7cb
ayaanmistry1807-eng <ayaanmistry1807@gmail.com>
2025-12-31 12:53:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,3 +70,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+// Navbar solid on scroll
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        document.querySelector('.header').classList.add('scrolled');
+    } else {
+        document.querySelector('.header').classList.remove('scrolled');
+    }
+});
